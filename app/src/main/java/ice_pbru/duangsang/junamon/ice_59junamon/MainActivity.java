a39eb157748d442c0b8d1a8e00f20fcd78d04193
junamon <fren_efull@hotmail.com>
2016-04-19 11:44:08 +0700

    หน้า listview

--- app/src/main/java/ice_pbru/duangsang/junamon/ice_59junamon/MainActivity.java
+++ app/src/main/java/ice_pbru/duangsang/junamon/ice_59junamon/MainActivity.java
@@ -1,5 +1,7 @@
 package ice_pbru.duangsang.junamon.ice_59junamon;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.facebook.com/fren.efull"));
+        startActivity(intent);
     }
 }
