035243db49c7752cd7e65e45f63ae6fb3cae2998
junamon <fren_efull@hotmail.com>
2016-04-20 09:56:34 +0700

    หน้า listview

--- app/src/main/java/ice_pbru/duangsang/junamon/ice_59junamon/MainActivity.java
+++ app/src/main/java/ice_pbru/duangsang/junamon/ice_59junamon/MainActivity.java
@@ -4,16 +4,36 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindWidget();
+        buttoncontroler();
 
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("https://www.facebook.com/fren.efull"));
-        startActivity(intent);
+
+    }
+
+    private void buttoncontroler() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.facebook.com/fren.efull"));
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void bindWidget() {
+        button = (Button) findViewById(R.id.button);
+
+
     }
 }
